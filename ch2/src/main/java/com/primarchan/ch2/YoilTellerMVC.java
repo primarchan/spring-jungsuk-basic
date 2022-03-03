1e94d48e9a2d6f357b23c2a38aff201c85e5fa23
Taegyun An <boltcarrier@naver.com>
2022-03-03 22:16:01 +0900

    YoilTellerMVC.java - update

--- ch2/src/main/java/com/primarchan/ch2/YoilTellerMVC.java
+++ ch2/src/main/java/com/primarchan/ch2/YoilTellerMVC.java
@@ -1,30 +1,34 @@
 package com.primarchan.ch2;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.Calendar;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class YoilTellerMVC {
-    @RequestMapping("/getYoilMVC") // http://localhost:8080/ch2/getYoilMVC?year=2021&month=10&day=1
-	public String main(int year, int month, int day, HttpServletResponse response) throws IOException {
-    	
-    	// 유효성 검사
-    	if(!isValid(year, month, day)) {
-    		return "yoilError";
-    	}
-    	
-        char yoil = getYoil(year, month, day);
-        
-        return "yoil"; // /WEB-INF/views/yoil.jsp
+	@RequestMapping("/getYoilMVC") // http://localhost:8080/ch2/getYoilMVC?year=2021&month=10&day=1
+	public String main(int year, int month, int day, Model model) throws IOException {
 
-    }
+		// 1. 유효성 검사
+		if (!isValid(year, month, day)) {
+			return "yoilError";
+		}
+
+		// 2. 요일 계산
+		char yoil = getYoil(year, month, day);
+
+		// 3. 계산한 결과를 Model에 저장
+		model.addAttribute("year", year);
+		model.addAttribute("month", month);
+		model.addAttribute("day", day);
+		model.addAttribute("yoil", yoil);
+
+		return "yoil"; // /WEB-INF/views/yoil.jsp
+
+	}
 
 	private boolean isValid(int year, int month, int day) {
 
